c5b208260ea9e13a1355230ac421cb042a75b2a9
rn7cvj <133586172+rn7cvj@users.noreply.github.com>
2024-01-06 00:06:10 +0300

    add locale

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,33 @@
 import { createApp } from 'vue'
-import './style.css'
+import  { createI18n } from 'vue-i18n'
 import App from './App.vue'
+import './style.css'
 
-createApp(App).mount('#app')
+const messages = {
+    en: {
+        message: {
+            hello: 'hello world'
+        }
+    },
+    ru: {
+        message: {
+            hello: 'привет мир'
+        }
+    }
+}
+
+
+
+const i18n = createI18n({
+
+    locale: "en",
+    fallbackLocale: "en",
+    messages
+
+})
+
+const app = createApp(App)
+
+app.use(i18n)
+
+app.mount('#app')
